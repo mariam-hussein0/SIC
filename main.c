7f6f75b03d51eacf28bd994e314681c250136721
SeifRedaMabrouk <redar1676@gmail.com>
2025-09-14 22:23:15 +0300

    Devision Process

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+float divide(int a, int b) {
+    if (b == 0) {
+        printf("Error: Division by zero!\n");
+        return 0; 
+    }
+    return (float)a / b ;
+}
+
+int main() {
+    int num1 = 10, num2 = 5;
+    float result;
+    result = divide(num1, num2);
+    printf("Result: %.2f\n", result);
     return 0;
 }
+
--- project.h
+++ project.h
@@ -1,6 +1,7 @@
 #ifndef PROJECT_H_INCLUDED
 #define PROJECT_H_INCLUDED
 
+float divide(int a, int b);
 
 
 #endif // PROJECT_H_INCLUDED
