f42c3d8ccf7bc17b4ced348e4436d2a48f6a13be
tawfik abdelhay <tawfikabdelhay23@gmail.com>
2025-09-14 22:16:02 +0300

    multiplication

---
+++ multiply.c
@@ -0,0 +1,23 @@
+
+// multi
+#include <stdio.h>
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    result = multiply(num1, num2);
+
+    printf("The multiplication of %d and %d is %d\n", num1, num2, result);
+
+    return 0;
+}
